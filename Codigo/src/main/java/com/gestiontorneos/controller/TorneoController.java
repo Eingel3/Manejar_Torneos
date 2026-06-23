1ffdfa49dd3f0e623a131b979c602cb947e31552
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-06-22 22:27:34 -0400

    Inicio de TorneoController con atibutos y metodo basico para crear torneos

--- Codigo/src/main/java/com/gestiontorneos/controller/TorneoController.java
+++ Codigo/src/main/java/com/gestiontorneos/controller/TorneoController.java
@@ -1,1 +1,43 @@
 package com.gestiontorneos.controller;
+
+//funcion: Gestionar todas las acciones relacionadas con los torneos
+public class TorneoController{
+
+    //guarda torneos existentes
+    private List<Torneo> torneos;
+
+    //es la lista de deportes
+    private List<Deporte> deportes;
+
+    //sirve para utilizar deportes existentes, eliminando la necesidad de crearlo de cero
+    private DeporteController deporteController;
+
+    //aca guarda los torneos con persistencia
+    private PersistenciaController persistenciaController;
+
+
+    public TorneoController(){
+
+    }
+
+    public Torneo crearTorneo(String nombreTorneo, String nombreDeporte){
+
+
+        //buscar deporte
+        Deporte deporte = deporteController.buscarDeporte(nombreDeporte);
+
+
+        //si no existe el deporte se crea
+        if ( deporte == null ){
+            deporte = deporteController.crearDeporte(nombreDeporte)
+        }
+
+        //se crea el torneo para el deporte
+        Torneo torneo = new Torneo(nombreTorneo, deporte);
+
+        //mientras no se use json no es necesario esto
+        //persistenciaController.guardarTorneo(torneos);
+    }
+     return torneo;
+}
+
