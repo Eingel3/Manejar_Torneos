cc8f30a8f4129ffda92ab2df5fec419d6726f426
Bastian <bvasquez2025@udec.cl>
2026-06-23 19:23:17 -0400

    Añadir imports y documentación javadoc a VentanaPrincipal

--- Codigo/src/main/java/com/gestiontorneos/gui/VentanaPrincipal.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/VentanaPrincipal.java
@@ -1,1 +1,18 @@
 package com.gestiontorneos.gui;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ * Panel principal de la interfaz gráfica de la app que maneja torneos.
+ * <p>
+ * Esta clase extiende {@link JPanel} e implementa {@link MouseListener} para
+ * capturar los clicks realizados por el usuario sobre la interfaz.
+ * </p>
+ * <p>
+ * El panel principal contiene y coordina dos subpaneles:
+ * </p>
+ */
+public class VentanaPrincipal extends JPanel implements MouseListener {
+
+}
