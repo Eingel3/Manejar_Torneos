19e0834a3c80eced9d757c3258da4035df6c04dd
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-06-25 02:35:03 -0400

    feat: Creada clase abstracta Participante con validaciones y métodos accesores

--- Codigo/src/main/java/com/gestiontorneos/model/participante/Participante.java
+++ Codigo/src/main/java/com/gestiontorneos/model/participante/Participante.java
@@ -1,1 +1,39 @@
 package com.gestiontorneos.model.participante;
+
+import com.gestiontorneos.model.excepciones.DatosInvalidosException;
+
+public abstract class Participante {
+
+    private final String rut; //TODO: Definir si utilizar rut o id
+    private final String nombre;
+    private final String contacto;
+
+    protected Participante(String nombre, String rut, String contacto) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            throw new DatosInvalidosException("El nombre del participante no puede estar vacío");
+        }
+        this.rut = rut;
+        this.nombre = nombre.trim();
+        this.contacto = contacto;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public abstract int getCantidadIntegrantes();
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
+
