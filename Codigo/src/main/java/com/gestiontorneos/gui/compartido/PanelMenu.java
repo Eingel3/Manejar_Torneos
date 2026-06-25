e8a430753078b9f70f4ab1ce57f568dceb747b06
Bastian <bvasquez2025@udec.cl>
2026-06-24 21:39:10 -0400

    Feat: Crear metodo crearBotonMenu en clase PanelMenu que crea botones para el menu de forma predeterminada

--- Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
@@ -9,4 +9,20 @@ public class PanelMenu extends JPanel {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
         this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));// Le ponemos un borde más claro para que se diferencie el mennú
     }
+
+    /**
+     *
+     * Metodo que se encarga de crear los botones que contiene el menu.
+     * Se utiliza para crear los botones uno a uno
+     *
+     * @return boton retorna un JButton con ciertos tamaños
+     */
+    private JButton crearBotonMenu(String nombre){
+    JButton boton = new JButton(nombre); //le damos su nombre
+    boton.setBackground(Color.PINK); //Le colocamos color rosa
+    boton.setAlignmentX(Component.CENTER_ALIGNMENT); //Le alineamos en el centro del menu
+    boton.setPreferredSize(new Dimension(40, 50)); //Le dejamos como un rectangulo un poquito mas chico que el ancho del menu
+    boton.setBorder(BorderFactory.createLineBorder(Color.MAGENTA));//Le añadimos un borde color magenta
+    return boton; //Y ya que hemos configurado el boton, podemos devolverlo
+    }
 }
