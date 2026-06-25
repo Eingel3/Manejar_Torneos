c815d7bee3f859cc5bcdd6d31b7dde623923198e
Bastian <bvasquez2025@udec.cl>
2026-06-24 21:18:53 -0400

    Inicializar el panel del menu lateral en el constructor de PanelMenu

--- Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
@@ -4,5 +4,9 @@ import java.awt.*;
 
 public class PanelMenu extends JPanel {
     public  PanelMenu() {
+        this.setBackground(Color.BLACK); //Color de fondo del panel
+        this.setPreferredSize(new Dimension(50, 800)); //Un poquit0 ancho + el largo de la VentanaPrincipal
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
+        this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));// Le ponemos un borde más claro para que se diferencie el mennú
     }
 }
