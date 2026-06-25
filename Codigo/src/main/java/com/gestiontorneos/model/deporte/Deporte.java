8b3c9a2f6b230f2b2c5d846886c64167821bf869
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-06-25 01:25:13 -0400

    feat: Creada clase Deporte con validaciones de datos y métodos accesores

--- Codigo/src/main/java/com/gestiontorneos/model/deporte/Deporte.java
+++ Codigo/src/main/java/com/gestiontorneos/model/deporte/Deporte.java
@@ -1,1 +1,36 @@
 package com.gestiontorneos.model.deporte;
+
+import com.gestiontorneos.model.excepciones.DatosInvalidosException;
+
+public class Deporte {
+
+    private final String nombre;
+    private final TipoParticipacion tipoParticipacion;
+
+    public Deporte(String nombre, TipoParticipacion tipoParticipacion) { //Constructor para crear un deporte
+        if (nombre == null || nombre.trim().isEmpty()) { //Validación de entradas
+            throw new DatosInvalidosException("El nombre del deporte no puede estar vacío");
+        }
+        if (tipoParticipacion == null) {
+            throw new DatosInvalidosException("El deporte debe tener un tipo de participación");
+        }
+
+        this.nombre = nombre.trim();
+        this.tipoParticipacion = tipoParticipacion;
+    }
+
+    public String getNombre() { //Getter para el nombre del deporte
+        return nombre;
+    }
+
+    public TipoParticipacion getTipoParticipacion() { //Getter para el tipo de participación
+        return tipoParticipacion;
+    }
+
+    @Override
+    public String toString() { //toString para mostrar el nombre del deporte
+        return nombre;
+    }
+
+
+}
