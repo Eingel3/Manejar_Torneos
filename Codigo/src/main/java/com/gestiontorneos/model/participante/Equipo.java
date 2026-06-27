e9d24974f373329568f61f1b9098c62a6a454748
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-06-27 14:47:15 -0400

    feat: Añadida clase Equipo con métodos para gestión de integrantes y validaciones

--- Codigo/src/main/java/com/gestiontorneos/model/participante/Equipo.java
+++ Codigo/src/main/java/com/gestiontorneos/model/participante/Equipo.java
@@ -1,1 +1,35 @@
 package com.gestiontorneos.model.participante;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import com.gestiontorneos.model.excepciones.DatosInvalidosException;
+
+public class Equipo extends Participante {
+
+    private final List<String> integrantes;
+
+    public Equipo(String nombre, String contacto, List<String> integrantes) {
+        super(nombre, contacto);
+        // toDo: Validaciones
+
+        this.integrantes = new ArrayList<>(integrantes);
+
+    }
+
+    @Override
+    public int getCantidadIntegrantes() {
+        return integrantes.size();
+    }
+
+    public List<String> getIntegrantes() { //toDo
+        return null;
+    }
+
+    public void agregarIntegrante(String nombreIntegrante) {
+        if(nombreIntegrante == null || nombreIntegrante.trim().isEmpty()) {
+            throw new DatosInvalidosException("El nombre no puede estar vacío");
+        }
+        integrantes.add(nombreIntegrante.trim());
+    }
+}
