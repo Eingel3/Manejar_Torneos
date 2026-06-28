9c89fe72ea006293807e99722c9d3566708b370f
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-06-28 19:36:05 -0400

    Metodos generales para deportesController

--- Codigo/src/main/java/com/gestiontorneos/controller/DeporteController.java
+++ Codigo/src/main/java/com/gestiontorneos/controller/DeporteController.java
@@ -4,5 +4,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class DeporteController {
-    
+
+    private list<Deporte> deportes;
+
+    public DeporteController(){
+        this.deportes = new ArrayList<>();
+    }
+
+    public Deporte buscarDeporte(){
+
+    }
+
+    public Deporte crearDeportes(){
+
+    }
+
+    public boolean eliminarDeporte(){
+
+    }
+
+    public List<Deporte> listaDeportes(){
+        return deportes;
+    }
+
 }
