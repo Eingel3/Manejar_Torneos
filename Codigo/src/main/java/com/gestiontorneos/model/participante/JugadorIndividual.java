5de231b84e8979c4156fe858b83fcdc5c85d19f3
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-06-28 16:39:41 -0400

    feat: Creada clase JugadorIndividual con implementación de método getCantidadIntegrantes

--- Codigo/src/main/java/com/gestiontorneos/model/participante/JugadorIndividual.java
+++ Codigo/src/main/java/com/gestiontorneos/model/participante/JugadorIndividual.java
@@ -1,1 +1,13 @@
 package com.gestiontorneos.model.participante;
+
+public class JugadorIndividual extends Participante {
+
+    public JugadorIndividual(String nombre, String contacto) {
+        super(nombre, contacto);
+    }
+
+    @Override
+    public int getCantidadIntegrantes() {
+        return 1;
+    }
+}
