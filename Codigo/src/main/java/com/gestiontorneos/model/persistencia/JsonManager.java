1943f811bc8ce743c1bdf40caad5cd7be07a3b28
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-06-29 02:55:28 -0400

    Base de JsonManager

--- Codigo/src/main/java/com/gestiontorneos/model/persistencia/JsonManager.java
+++ Codigo/src/main/java/com/gestiontorneos/model/persistencia/JsonManager.java
@@ -1,1 +1,24 @@
 package com.gestiontorneos.model.persistencia;
+
+import com.google.gson.Gson;//Convertir objetos Java a JSON y viceversa
+//import java.io.FileReader;//Leer un archivo Json
+//import java.io.FileWriter;//Escribir un archivo Json
+//import java.io.IOException;//Manejar errores de lectura y escritura
+import com.google.gson.GsonBuilder;//para configurar gson
+import java.lang.reflect.Type;//sirve para  cargar listas
+import com.google.gson.reflect.TypeToken;//trabaja con Type para indicar el tipo
+import java.io.*;//implementa todos los import relacionados con los archivos
+
+
+public class JsonManager{
+
+    Gson gson = new Gson();
+
+    public JsonManager(){
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void guardar(){
+        
+    }
+}
