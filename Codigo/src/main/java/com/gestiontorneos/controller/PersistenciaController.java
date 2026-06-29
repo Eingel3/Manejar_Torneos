4d51c80c26df5318b69a8649084952ae4f2468cf
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-06-29 01:49:37 -0400

    Inicio en PersistenciaController

--- Codigo/src/main/java/com/gestiontorneos/controller/PersistenciaController.java
+++ Codigo/src/main/java/com/gestiontorneos/controller/PersistenciaController.java
@@ -1,1 +1,19 @@
 package com.gestiontorneos.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import com.gestiontorneos.model.torneo.Torneo;
+import com.gestiontorneos.model.deporte.Deporte;
+import com.gestiontorneos.model.persistencia.JsonManager;
+
+
+public class PersistenciaController{
+
+    private JsonManager jsonManager;
+
+    public  PersistenciaController(){
+        this.jsonManager = new JsonManager();
+    }
+
+    
+}
