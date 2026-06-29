c446bd3f99d67a83bf8ecc86998b78e8fad49d1e
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-06-29 04:13:49 -0400

    metodo para deserializar en jsonmanager

--- Codigo/src/main/java/com/gestiontorneos/model/persistencia/JsonManager.java
+++ Codigo/src/main/java/com/gestiontorneos/model/persistencia/JsonManager.java
@@ -30,5 +30,19 @@ public class JsonManager{
             e.printStackTrace();
         }
 
+        //el <T> T significa que es una lista generica pero una lista dentro de todo
+        public <T> T deserializar(String archivo){
+
+            try(FileReader reader = new FileReader(archivo)){
+
+                return gson.fromJson(reader);
+
+            }catch(IOexception e){
+                System.err.println("Error al cargar el archivo.");
+                //print enfocado en errores detallados
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
