a85d20eb5c00f683514816fa6d7fdb67cb196696
Bastian <bvasquez2025@udec.cl>
2026-06-29 01:05:27 -0400

    Feat: Añadir metodos getNombre y agregarParticipante en la clase Torneo (de forma básica)

--- Codigo/src/main/java/com/gestiontorneos/model/torneo/Torneo.java
+++ Codigo/src/main/java/com/gestiontorneos/model/torneo/Torneo.java
@@ -1,5 +1,23 @@
 package com.gestiontorneos.model.torneo;
 
+import com.gestiontorneos.model.participante.Participante;
+
+/**
+ * Clase editada para que no de problemas por ahora
+ * Es decir, está incompleta
+ */
+
 public class Torneo {
+    private String nombre;
     public Torneo() {}
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void agregarParticipante(Participante participante){
+        if ( participante != null ){
+            //codigo
+        }
+    }
 }
