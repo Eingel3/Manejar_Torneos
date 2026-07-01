e3d2d229b6e347cb5744f7d2c2204bc9aa78993a
Bastian Vásquez <bvasquez2025@udec.cl>
2026-07-01 05:37:04 -0400

    Feat: Crear clase tipo enum PanelInformacion quien contiene información acerca de los paneles (alto y ancho dde cada panel)

---
+++ Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelInformacion.java
@@ -0,0 +1,20 @@
+package com.gestiontorneos.gui.compartido;
+
+public enum PanelInformacion {
+    VENTANAPRINCIPAL(1200, 800),
+    MENULATERAL(100, 800),
+    VENTANASINMENU(1100, 800);
+
+    private final int ancho;
+    private final int alto;
+    PanelInformacion(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+    public int getAncho() {
+        return ancho;
+    }
+    public int getAlto() {
+        return alto;
+    }
+}
