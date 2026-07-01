deb971faf04941f2a26d5ffeb0b5c8829ca6ef8c
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-07-01 00:58:21 -0400

    Creacion de la base para la class Partido

--- Codigo/src/main/java/com/gestiontorneos/model/partido/Partido.java
+++ Codigo/src/main/java/com/gestiontorneos/model/partido/Partido.java
@@ -1,1 +1,39 @@
 package com.gestiontorneos.model.partido;
+
+import com.gestiontorneos.model.torneo.Torneo;
+
+public class Partido{
+
+    private String nombre;
+    private Torneo torneo;
+    private EstadoPartido estado;
+
+    public Partido(String nombre, Torneo torneo){
+        this.nombre = nombre;
+        this.torneo = torneo;
+    }
+
+    //GETTERS
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    //por mientras getTorneo que es mas generico pero util
+    public Torneo getTorneo(){
+        return torneo;
+    }
+
+    public EstadoPartido getEstadoPartido(){
+        return estado;
+    }
+
+
+    //SETTERS
+
+
+    public void setEstadoPartido(EstadoPartido estado){
+        this.estado = estado;
+    }
+
+}
