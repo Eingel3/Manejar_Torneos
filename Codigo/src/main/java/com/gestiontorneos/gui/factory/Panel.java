40266acbc33624b968f956c6b21399896fa8938c
Bastian <bvasquez2025@udec.cl>
2026-07-01 20:00:20 -0400

    Feat: Crear clase SUbPanel que implementa a Panel y crewa un JPanel generico

--- Codigo/src/main/java/com/gestiontorneos/gui/factory/Panel.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/Panel.java
@@ -3,5 +3,5 @@ package com.gestiontorneos.gui.factory;
 import javax.swing.*;
 
 public interface Panel {
-    public JPanel crear; //Metodo usado para crear un JPanel
+    public JPanel crear(); //Metodo usado para crear un JPanel
 }
---
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/SubPanel.java
@@ -0,0 +1,20 @@
+package com.gestiontorneos.gui.factory;
+
+import com.gestiontorneos.gui.compartido.PanelInformacion;
+
+import javax.swing.*;
+import java.awt.*;
+
+//Este metodo crea el panel donde se muestran la lista de torneos, o el calendario, y en general todas las pestañas
+public class SubPanel extends JPanel implements Panel {
+    @Override public JPanel crear(){
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.PINK); //definimos el color de fondo
+        panel.setLayout(new FlowLayout());//definimos el layout
+        panel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY)); //Definimos un borde
+        panel.setPreferredSize(new Dimension(//Y definimos las dimensiones
+                PanelInformacion.VENTANASINMENU.getAncho() - 10, //Le quitamos 10 para una correcta implementación
+                PanelInformacion.VENTANASINMENU.getAlto()));
+        return panel; //Finalmente retornamos el panel
+    }
+}
