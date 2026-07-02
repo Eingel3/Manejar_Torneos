f79dd33bc9f34af0c40824a1f9df676235665bb0
Bastian <bvasquez2025@udec.cl>
2026-07-01 23:36:44 -0400

    Feat: Realizar override al metodo crear dentro de PanelLateral para los casos de ancho o ancho y alto personalizados

--- Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelLateral.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelLateral.java
@@ -12,7 +12,31 @@ public class PanelLateral implements PanelFactory{
         panel.setBackground(Color.BLACK); //Color de fondo del panel
         panel.setPreferredSize(new Dimension(
                 PanelInformacion.MENULATERAL.getAncho(),
-                PanelInformacion.MENULATERAL.getAlto())); //Un poquit0 ancho + el largo de la VentanaPrincipal
+                PanelInformacion.MENULATERAL.getAlto())); //las dimensiones son determinadas por el enum PanelInformacion
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
+
+        return  panel;
+    }
+    @Override
+    public JPanel crear(int ancho){
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK); //Color de fondo del panel
+        panel.setPreferredSize(new Dimension(
+                ancho,
+                PanelInformacion.MENULATERAL.getAlto()));
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
+
+        return  panel;
+    }
+    @Override
+    public JPanel crear(int ancho, int alto){
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK); //Color de fondo del panel
+        panel.setPreferredSize(new Dimension(
+                ancho,
+                alto));
 
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
 
