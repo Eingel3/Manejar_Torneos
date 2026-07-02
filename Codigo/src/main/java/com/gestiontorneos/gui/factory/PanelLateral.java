57baac38674e2fb4a2fecceb9823895f7a923f22
Bastian <bvasquez2025@udec.cl>
2026-07-02 00:26:57 -0400

    Añadir documentacion tipo javaDoc a la clase PanelLateral

--- Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelLateral.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelLateral.java
@@ -5,7 +5,16 @@ import com.gestiontorneos.gui.compartido.PanelInformacion;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * clase que implementa al PanelFactory.
+ *  Se utliza para crear un panel angosto, de layout tipo Box, disposicion vertical.
+ *  Gracias a la sobrecarga del metodo crear se puede adaptar a distintos usos.
+ */
 public class PanelLateral implements PanelFactory{
+    /**
+     * Override del metodo crear, esta funcion crea un JPanel lateral predeterminado
+     * @return retorna un JPanel angosto, de layout tipo Box, disposicion vertical.
+     */
     @Override
     public JPanel crear(){
         JPanel panel = new JPanel();
@@ -13,11 +22,16 @@ public class PanelLateral implements PanelFactory{
         panel.setPreferredSize(new Dimension(
                 PanelInformacion.MENULATERAL.getAncho(),
                 PanelInformacion.MENULATERAL.getAlto())); //las dimensiones son determinadas por el enum PanelInformacion
-
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
-
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar las adiciones
         return  panel;
     }
+
+    /**
+     *   Override del metodo crear, esta funcion crea un JPanel lateral predeterminado
+     *
+     * @param ancho es el ancho que se desea que tenga el JPanel lateral
+     * @return Retorna un JPanel de ancho personalizado, dado por la variable ancho, de layout tipo Box, disposicion vertical.
+     */
     @Override
     public JPanel crear(int ancho){
         JPanel panel = new JPanel();
@@ -25,11 +39,17 @@ public class PanelLateral implements PanelFactory{
         panel.setPreferredSize(new Dimension(
                 ancho,
                 PanelInformacion.MENULATERAL.getAlto()));
-
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
-
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar las adiciones
         return  panel;
     }
+
+    /**
+     *   Override del metodo crear, esta funcion crea un JPanel lateral predeterminado
+     *
+     * @param ancho es el ancho que se desea que tenga el JPanel lateral
+     * @param alto es el alto que se desea que tenga el JPanel lateral
+     * @return Retorna un JPanel de ancho  y alto personalizados, dados por las variables ancho y alto, de layout tipo Box, disposicion vertical.
+     */
     @Override
     public JPanel crear(int ancho, int alto){
         JPanel panel = new JPanel();
@@ -37,9 +57,7 @@ public class PanelLateral implements PanelFactory{
         panel.setPreferredSize(new Dimension(
                 ancho,
                 alto));
-
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
-
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar las adiciones
         return  panel;
     }
 }
