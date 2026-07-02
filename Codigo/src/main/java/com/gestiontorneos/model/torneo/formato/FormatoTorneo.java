4d1b44b96ae1169fa999b79f341ed26c24ff7e5c
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-07-01 20:02:27 -0400

    feat: Creada interfaz FormatoTorneo con métodos para gestión de formatos y resultados

--- Codigo/src/main/java/com/gestiontorneos/model/torneo/formato/FormatoTorneo.java
+++ Codigo/src/main/java/com/gestiontorneos/model/torneo/formato/FormatoTorneo.java
@@ -1,1 +1,22 @@
 package com.gestiontorneos.model.torneo.formato;
+
+import com.gestiontorneos.model.participante.Participante;
+import com.gestiontorneos.model.partido.Partido;
+import com.gestiontorneos.model.partido.Calendario;
+import com.gestiontorneos.model.torneo.Clasificacion;
+import java.util.List;
+
+public interface FormatoTorneo {
+
+    //Genera todos los partidos iniciales según los participantes inscritos
+    List<Partido> generarEnfrentamientos(List<Participante> participantes);
+
+    //Recalcula la tabla cuando un partido termina
+    void actualizarClasificacion(Clasificacion clasificacion, Partido partido);
+
+    // Variable para determinar si terminó el torneo
+    boolean haTerminado(Calendario calendario);
+
+    //Se obtiene el ganador
+    Participante obtenerGanador(Calendario calendario, Clasificacion clasificacion);
+}
