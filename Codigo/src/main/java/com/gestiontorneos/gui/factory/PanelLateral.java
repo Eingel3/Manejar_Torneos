839483245a7e47082f6d978710b4ac4b92600abd
Bastian <bvasquez2025@udec.cl>
2026-07-01 23:11:57 -0400

    Crear fabrica de paneles laterales, clase PanelLateral que implementa la interfaz PanelFactory

---
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelLateral.java
@@ -0,0 +1,19 @@
+package com.gestiontorneos.gui.factory;
+
+import com.gestiontorneos.gui.compartido.PanelInformacion;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelLateral implements PanelFactory{
+    @Override
+    public JPanel crear(){
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK); //Color de fondo del panel
+        panel.setPreferredSize(new Dimension(
+                PanelInformacion.MENULATERAL.getAncho(),
+                PanelInformacion.MENULATERAL.getAlto())); //Un poquit0 ancho + el largo de la VentanaPrincipal
+
+        panel.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
+    }
+}
