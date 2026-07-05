6538c951fc42a5a721fcead0ae99c7d855d0cddc
Bastian <bvasquez2025@udec.cl>
2026-07-05 02:28:04 -0400

    Fix: Dentro de los setters de la clase PanelFuturosEventos arreglar errores de typeo e iniciar el gbc correctamente en el caso de que el ciclo for no encuentre a un componente con el gbc buscado

--- Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelFuturosEventos.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelFuturosEventos.java
@@ -80,6 +80,14 @@ public class PanelFuturosEventos extends JPanel{
                 break;
             }
         }
+
+        //Si resulta que en el lugar de la celda que se quiere colocar un nuevo evento no existe un evento anterior entonces inicializamos el gbc de forma manual
+        if (gbc == null) {
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = id;
+        }
+
         //Lo siguiente maneja el como los componentes ocupan el espacio
         //En este caso, los paneles usan todo el espacio disponible
         gbc.weightx = 1;
@@ -107,15 +115,23 @@ public class PanelFuturosEventos extends JPanel{
         datosPanel.setBackground(Color.cyan);
         datosPanel.add(datosLabel);
         //Ahora removemos el evento que ya esté en el id
-        GridBagLayout layout = (GridBagLayout) futurosTorneos.getLayout(); //en el layout
+        GridBagLayout layout = (GridBagLayout) futurosPartidos.getLayout(); //en el layout
         GridBagConstraints gbc;
-        for (Component comp : futurosTorneos.getComponents()) { //Revisamos cada componente
+        for (Component comp : futurosPartidos.getComponents()) { //Revisamos cada componente
             gbc = layout.getConstraints(comp); //Asignamos el gbc segun los datos del componente
             if (gbc.gridx == 0 && gbc.gridy == id) { //Revisamos que el componente esté dentro de la celda que queremos cambiar
-                futurosTorneos.remove(comp); //Y removemos el componente para reemplazarlo
+                futurosPartidos.remove(comp); //Y removemos el componente para reemplazarlo
                 break;
             }
         }
+
+        //Si resulta que en el lugar de la celda que se quiere colocar un nuevo evento no existe un evento anterior entonces inicializamos el gbc de forma manual
+        if (gbc == null) {
+            gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = id;
+        }
+
         //Lo siguiente maneja el como los componentes ocupan el espacio
         //En este caso, los paneles usan todo el espacio disponible
         gbc.weightx = 1;
@@ -123,9 +139,9 @@ public class PanelFuturosEventos extends JPanel{
         gbc.fill = GridBagConstraints.BOTH;
 
         //Finalmente agregamos el datosPanel
-        futurosTorneos.add(datosPanel, gbc);
+        futurosPartidos.add(datosPanel, gbc);
         //Y revalidamos y repintamos para que los cambios sean correctamente actualizados
-        futurosTorneos.revalidate();
-        futurosTorneos.repaint();
+        futurosPartidos.revalidate();
+        futurosPartidos.repaint();
     }
 }
