75e02dd3d435b03de88a338c6856e7c35287dce1
Bastian <bvasquez2025@udec.cl>
2026-07-05 15:46:05 -0400

    Feat: Dentro de PanelMenu crear un boton para los futuros eventos, crear un metodo que maneje la creación de botones

--- Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/compartido/PanelMenu.java
@@ -12,35 +12,50 @@ public class PanelMenu extends JPanel {
     JButton partidos;
     JButton inicio;
 
-    public  PanelMenu() {
-        this.setBackground(Color.BLACK); //Color de fondo del panel
-        this.setPreferredSize(new Dimension(
-                PanelInformacion.MENULATERAL.getAncho(),
-                PanelInformacion.MENULATERAL.getAlto())); //Un poquit0 ancho + el largo de la VentanaPrincipal
+    public class PanelMenu extends JPanel {
+        JButton calendario;
+        JButton torneos;
+        JButton clasificaciones;
+        JButton partidos;
+        JButton inicio;
+        JButton futurosEventos;
 
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
-        this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));// Le ponemos un borde más claro para que se diferencie el mennú
+        public  PanelMenu() {
+            this.setBackground(Color.BLACK); //Color de fondo del panel
+            this.setPreferredSize(new Dimension(
+                    PanelInformacion.MENULATERAL.getAncho(),
+                    PanelInformacion.MENULATERAL.getAlto())); //Un poquit0 ancho + el largo de la VentanaPrincipal
 
-        BotonFactory crearBoton = new BotonSimple();
+            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));//Usamos BoxLayout de forma vertical para colocar los botones del menu lateral
+            this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));// Le ponemos un borde más claro para que se diferencie el mennú
 
-        inicio = crearBoton.crear("Inicio"); //creamos un boton para la pestaña de inicio
-        this.add(inicio); //y lo agregamos al panel
-        add(Box.createRigidArea(new Dimension(0, 30))); //Luego, agregamos un espacio entre este boton y el siguiente
+            crearBotones();
 
-        /**
-         * Repetimos el proceso de arriba con todos los otros botones, creamos el boton, lo agrgamos al panel, agregamos un espaciado y creamos el siguiente boton
-         */
-        calendario = crearBoton.crear("Calendario");
-        this.add(calendario);
-        add(Box.createRigidArea(new Dimension(0, 30)));
-        torneos = crearBoton.crear("Torneos");
-        this.add(torneos);
-        add(Box.createRigidArea(new Dimension(0, 30)));
-        clasificaciones = crearBoton.crear("Clasificaciones");
-        this.add(clasificaciones);
-        add(Box.createRigidArea(new Dimension(0, 30)));
-        partidos = crearBoton.crear("Partidos");
-        this.add(partidos);
+        }
+
+        private void crearBotones(){
+            BotonFactory crearBoton = new BotonSimple();
+
+            inicio = crearBoton.crear("Inicio"); //creamos un boton para la pestaña de inicio
+            this.add(inicio); //y lo agregamos al panel
+            add(Box.createRigidArea(new Dimension(0, 30))); //Luego, agregamos un espacio entre este boton y el siguiente
+
+            /**
+             * Repetimos el proceso de arriba con todos los otros botones, creamos el boton, lo agrgamos al panel, agregamos un espaciado y creamos el siguiente boton
+             */
+            calendario = crearBoton.crear("Calendario");
+            this.add(calendario);
+            add(Box.createRigidArea(new Dimension(0, 30)));
+            torneos = crearBoton.crear("Torneos");
+            this.add(torneos);
+            add(Box.createRigidArea(new Dimension(0, 30)));
+            clasificaciones = crearBoton.crear("Clasificaciones");
+            this.add(clasificaciones);
+            add(Box.createRigidArea(new Dimension(0, 30)));
+            partidos = crearBoton.crear("Partidos");
+            this.add(partidos);
+            futurosEventos = crearBoton.crear("Futuros Eventos");
+            this.add(futurosEventos);
+        }
+
     }
-
-}
