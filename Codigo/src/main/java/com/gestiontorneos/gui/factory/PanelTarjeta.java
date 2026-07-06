08a6072f8b01b42a21b1ddbfd91a275ccb29b9fa
Bastian <bvasquez2025@udec.cl>
2026-07-06 02:24:22 -0400

    Editar colores de PanelTarjeta

--- Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelTarjeta.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/factory/PanelTarjeta.java
@@ -27,7 +27,7 @@ public class PanelTarjeta implements PanelFactory {
     @Override
     public JPanel crear() {
         JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK); //Color de fondo del panel
+        panel.setBackground(Color.PINK); //Color de fondo del panel
         panel.setPreferredSize(new Dimension(
                 PanelInformacion.TARJETA.getAncho(),
                 PanelInformacion.TARJETA.getAlto())); //las dimensiones son determinadas por el enum PanelInformacion
@@ -37,7 +37,7 @@ public class PanelTarjeta implements PanelFactory {
         panel.setMaximumSize(new Dimension( //Y un tamaño maximo para evitar comportamientos no deseados
                 PanelInformacion.TARJETA.getAncho(),
                 PanelInformacion.TARJETA.getAlto()));
-        panel.setBorder(BorderFactory.createLineBorder(Color.PINK));
+        panel.setBorder(BorderFactory.createLineBorder(Color.WHITE));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); //Usamos BoxLayout de forma vertical para colocar las adiciones
         return panel;
     }
@@ -51,7 +51,7 @@ public class PanelTarjeta implements PanelFactory {
     @Override
     public JPanel crear(int alto) {
         JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK); //Color de fondo del panel
+        panel.setBackground(Color.PINK); //Color de fondo del panel
         panel.setPreferredSize(new Dimension(
                 PanelInformacion.TARJETA.getAncho(),
                 alto));
@@ -61,7 +61,7 @@ public class PanelTarjeta implements PanelFactory {
         panel.setMaximumSize(new Dimension( //Y un tamaño maximo para evitar comportamientos no deseados
                 PanelInformacion.TARJETA.getAncho(),
                 PanelInformacion.TARJETA.getAlto()));
-        panel.setBorder(BorderFactory.createLineBorder(Color.PINK));
+        panel.setBorder(BorderFactory.createLineBorder(Color.WHITE));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); //Usamos BoxLayout de forma vertical para colocar las adiciones
         return panel;
     }
