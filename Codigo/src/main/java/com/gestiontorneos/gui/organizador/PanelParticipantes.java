18d125904c00069b7c1957b798efdf9d5bab5e08
Bastian <bvasquez2025@udec.cl>
2026-07-06 01:24:47 -0400

    Crear clase PanelParticipantes, crear su constructor y un metodo para agregar un participante

--- Codigo/src/main/java/com/gestiontorneos/gui/organizador/PanelParticipantes.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/organizador/PanelParticipantes.java
@@ -1,6 +1,10 @@
 package com.gestiontorneos.gui.organizador;
+import com.gestiontorneos.gui.compartido.PanelInformacion;
+import com.gestiontorneos.gui.factory.PanelTarjeta;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 /**
  * Representa la vista grafica de los participantes
@@ -9,7 +13,38 @@ import java.awt.*;
  */
 
 public class PanelParticipantes extends JPanel {
-    public PanelParticipantes() {
+
+    public PanelParticipantes(){
+            //Ajustes del JPanel
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //definimos el layout
+            setBackground(Color.lightGray); //definimos el color de fondo
+            setPreferredSize(new Dimension(
+                    PanelInformacion.VENTANASINMENU.getAncho(),
+                    PanelInformacion.VENTANASINMENU.getAlto()));//Y las dimensiones
+
+        }
+        public void agregarParticipanteGUI(String nombre, String enQueParticipa, String esUnEquipoOUnIndividuo){
+
+            PanelTarjeta creadorTarjeta = new PanelTarjeta();
+            JPanel torneoGUI = creadorTarjeta.crear(500, 150); //Aquí es donde dejaremos todos los datos
+            torneoGUI.setLayout(new FlowLayout(FlowLayout.LEFT));//layout tipo Flow que agrega cada componente de izquierda a derecha
+            //Creamos los distintos labels para cada informacion que mostremos
+            JLabel lblNombre = new JLabel(nombre);
+            JLabel lblParticipa = new JLabel(enQueParticipa);
+            JLabel lblQueEs = new JLabel(esUnEquipoOUnIndividuo);
+            //Ahora le asignamos una fuente y el tamaño a los labels
+            lblNombre.setFont(new Font("Dialog", Font.BOLD, 18)); //este es mas grando
+            lblParticipa.setFont(new Font("Dialog", Font.BOLD, 14));
+            lblQueEs.setFont(new Font("Dialog", Font.BOLD, 14));
+            //Y agregamos los labels a torneoGUI
+            torneoGUI.add(lblNombre);
+            torneoGUI.add(lblParticipa);
+            torneoGUI.add(lblQueEs);
+
+            this.add(torneoGUI);//añadimos al JPanel de PanelListaTorneos
+            this.revalidate();
+            this.repaint();
+        }
 
     }
 }
