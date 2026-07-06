a809652deccdc9661c833f2f68e38888e3f8a288
Bastian <bvasquez2025@udec.cl>
2026-07-06 16:43:48 -0400

    Feat: Crear metodos internos de PanelCrearPartido para crear un partido y los getters necesarios

--- Codigo/src/main/java/com/gestiontorneos/gui/organizador/PanelCrearPartido.java
+++ Codigo/src/main/java/com/gestiontorneos/gui/organizador/PanelCrearPartido.java
@@ -17,7 +17,8 @@ import java.awt.*;
  * @see JPanel
  */
 public class PanelCrearPartido extends JPanel {
-    private JTextField txtNombreParticipante;
+    private JTextField txtNombreParticipanteLocal;
+    private JTextField txtNombreParticipanteVisitante;
     private JComboBox<String> tipoParticipante; //Si es local o visitante
     private JTextField txtNombreTorneo;
     private JButton btnCrear;
@@ -35,5 +36,100 @@ public class PanelCrearPartido extends JPanel {
                 PanelInformacion.VENTANASINMENU.getAncho(),
                 PanelInformacion.VENTANASINMENU.getAlto()));
         creadorBotones = new BotonSimple();
+    }
+    public void crearPartido() {
+        JLabel lblTitulo = new JLabel("Crear Nuevo Partido");
+        lblTitulo.setFont(new Font("Dialog", Font.BOLD, 24));
+        lblTitulo.setForeground(Color.BLACK);
+        lblTitulo.setAlignmentX(CENTER_ALIGNMENT);
+
+        this.add(new JLabel("Indique el torneo al que se quiere agregar el partido:"));
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+        this.add(txtNombreTorneo = new JTextField());
+        this.add(Box.createRigidArea(new Dimension(0, 60)));
+
+        this.add(new JLabel("Nombre del participante o equipo marcado como 'Local':"));
+        this.add(txtNombreParticipanteLocal = new JTextField());
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        this.add(new JLabel("Nombre del participante o equipo marcado como 'Visitante':"));
+        this.add(txtNombreParticipanteVisitante = new JTextField());
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+
+
+        this.add(new JLabel("Indique el estado del partido:"));
+
+
+
+
+        this.add(btnCrear = creadorBotones.crear("Crear"));
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        this.add(btnCancelar = creadorBotones.crear("Cancelar"));
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+    }
+
+
+    public void elegirTorneo() {
+        this.add(new JLabel("Nombre del torneo en que participa:"));
+
+
+        this.add(btnCrear = creadorBotones.crear("Crear"));
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        this.add(btnCancelar = creadorBotones.crear("Cancelar"));
+        this.add(Box.createRigidArea(new Dimension(0, 30)));
+    }
+
+
+    public JButton getBotonCrear() {
+        return btnCrear;
+    }
+
+    public JButton getBotonCancelar() {
+        return btnCancelar;
+    }
+
+    public String getNombreTorneo() {
+        return txtNombreTorneo.getText().trim();
+    }
+
+    public String getNombreParticipanteLocal() {
+        return (String) txtNombreParticipanteLocal.getText().trim();
+    }
+
+        public String getNombreParticipanteVisitante() {
+        return (String) txtNombreParticipanteVisitante.getText().trim();
+    }
+
+    public String getTipoParticipante() {
+        return (String) tipoParticipante.getSelectedItem();
+    }
+
+
+    public void limpiarFormulario() {
+        txtNombreParticipanteLocal.setText("");
+        txtNombreParticipanteVisitante.setText("");
+        tipoParticipante.setSelectedIndex(0);
+        txtNombreTorneo.setText("");
+        this.revalidate();
+        this.repaint();
+    }
+
+    public void limpiarPanel() {
+        this.removeAll();
+        this.revalidate();
+        this.repaint();
+    }
+
+    public void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
+    public boolean confirmar(String pregunta){
+        int respuesta = JOptionPane.showConfirmDialog(this,pregunta,
+                "Advertencia", JOptionPane.YES_NO_OPTION);
+        return  respuesta == JOptionPane.YES_OPTION;
     }
 }
