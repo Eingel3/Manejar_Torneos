044bf92ff7d21467b75d50da324c30ae18941080
Bsaavedra <Bsaavedra2024@inf.udec.cl>
2026-07-08 17:41:31 -0400

    metodo en torneocontroller para generarcalendario

--- Codigo/src/main/java/com/gestiontorneos/controller/TorneoController.java
+++ Codigo/src/main/java/com/gestiontorneos/controller/TorneoController.java
@@ -132,6 +132,21 @@ public class TorneoController{
         return new ArrayList<>();
     }
 
+    public boolean generarCalendario(String nombreTorneo){
+        Torneo torneo = buscarTorneo(nombreTorneo);
+        if(torneo == null){
+            return false;
+        }else{
+            try{
+                torneo.generarCalendario();
+                return true;
+            }catch(Exception e){
+                System.err.println("Error al generar calendario " + e.getMessage());
+                return false;
+            }
+        }
+    }
+
 
 
 
